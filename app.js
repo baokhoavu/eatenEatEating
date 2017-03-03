b6bf956965613d1422d51c1f352f4d4714a5c57d
Baokhoa Vu <tealkrysta92@gmail.com>
2017-03-02 16:23:06 -0800

    favicon added

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
