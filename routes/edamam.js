094bb1c0b40823d9d2d51740f253b0dcf65e0128
Baokhoa Vu <tealkrysta92@gmail.com>
2017-03-02 23:29:18 -0800

    lalallaa

--- routes/edamam.js
+++ routes/edamam.js
@@ -12,7 +12,7 @@ router.post('/cut', function(req, res, next) {
   var query = req.body.searchDataOne
   var app_id = process.env.EDAMAM_APP_ID;
   var app_key = process.env.EDAMAM_API_KEY;
-  var url = `https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}&diet=low-fat&from=0&to=20&calories=gte%20200,%20lte%20225&health=alcohol-free`
+  var url = `http://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}&diet=low-fat&from=0&to=20&calories=gte%20200,%20lte%20225&health=alcohol-free`
   request(url, function(err, response, body) {
     if (!err && res.statusCode == 200){
       try {
@@ -29,7 +29,7 @@ router.post('/bulk', function(req, res, next) {
   var query = req.body.searchDataTwo
   var app_id = process.env.EDAMAM_APP_ID;
   var app_key = process.env.EDAMAM_API_KEY;
-  var url = `https://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}&diet=high-protein&from=0&to=20&calories=gte%20200,%20lte%20500&health=alcohol-free`
+  var url = `http://api.edamam.com/search?q=${query}&app_id=${app_id}&app_key=${app_key}&diet=high-protein&from=0&to=20&calories=gte%20200,%20lte%20500&health=alcohol-free`
   request(url, function(err, response, body) {
     if (!err && res.statusCode == 200){
       try {
